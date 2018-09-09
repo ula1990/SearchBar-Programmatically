8e87bdc0a9d929d47a3b616370a9ae01983723e5
ula1990 <uladzislau.daratsiuk@gmail.com>
2018-09-09 09:40:20 +0200

    Initial Commit

---
+++ SearchBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchBar
+//
+//  Created by Uladzislau Daratsiuk on 9/9/18.
+//  Copyright © 2018 Uladzislau Daratsiuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
